768c50cf37b20f56194e9a0174d9888de132c39d
Francis Yeong <33077075+fywk@users.noreply.github.com>
2022-11-15 13:44:34 +0800

    Add custom favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
         <link
           href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400;500;600;700&display=swap"
           rel="stylesheet"
