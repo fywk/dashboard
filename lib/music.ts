7bd91211702f1cf737e23b27ced7544a6aadcb8c
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-01-17 18:54:21 +0800

    Change`NEXT_PUBLIC_VERCEL_URL` to `NEXT_PUBLIC_URL`

--- lib/music.ts
+++ lib/music.ts
@@ -3,14 +3,14 @@ import { ArtistImage } from "./types/spotify";
 
 export const fetchArtistAvatar = async (name: string): Promise<ArtistImage> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/music/artist-image?name=${name}`
+    `${process.env.NEXT_PUBLIC_URL}/api/music/artist-image?name=${name}`
   );
   return res.json();
 };
 
 export const fetchTopArtists = async (): Promise<TopArtists> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/music/top-artists?period=1month`,
+    `${process.env.NEXT_PUBLIC_URL}/api/music/top-artists?period=1month`,
     { cache: "no-store" }
   );
   return res.json();
@@ -18,7 +18,7 @@ export const fetchTopArtists = async (): Promise<TopArtists> => {
 
 export const fetchTopAlbums = async (): Promise<TopAlbums> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/music/top-albums?period=3month`,
+    `${process.env.NEXT_PUBLIC_URL}/api/music/top-albums?period=3month`,
     { cache: "no-store" }
   );
   return res.json();
@@ -26,7 +26,7 @@ export const fetchTopAlbums = async (): Promise<TopAlbums> => {
 
 export const fetchTotalPlays = async (): Promise<RecentTrack> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/music/recent-track`,
+    `${process.env.NEXT_PUBLIC_URL}/api/music/recent-track`,
     { cache: "no-store" }
   );
   return res.json();
@@ -34,7 +34,7 @@ export const fetchTotalPlays = async (): Promise<RecentTrack> => {
 
 export const fetchTotalTracks = async (): Promise<TotalStats> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/music/track-total?period=7day`,
+    `${process.env.NEXT_PUBLIC_URL}/api/music/track-total?period=7day`,
     { cache: "no-store" }
   );
   return res.json();
@@ -42,7 +42,7 @@ export const fetchTotalTracks = async (): Promise<TotalStats> => {
 
 export const fetchTotalAlbums = async (): Promise<TotalStats> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/music/album-total?period=7day`,
+    `${process.env.NEXT_PUBLIC_URL}/api/music/album-total?period=7day`,
     { cache: "no-store" }
   );
   return res.json();
@@ -50,7 +50,7 @@ export const fetchTotalAlbums = async (): Promise<TotalStats> => {
 
 export const fetchTotalArtists = async (): Promise<TotalStats> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/music/artist-total?period=7day`,
+    `${process.env.NEXT_PUBLIC_URL}/api/music/artist-total?period=7day`,
     { cache: "no-store" }
   );
   return res.json();
--- lib/weather.ts
+++ lib/weather.ts
@@ -2,7 +2,7 @@ import { CurrentWeatherData, ForecastWeatherData } from "./types/openweather";
 
 export const fetchCurrentWeather = async (): Promise<CurrentWeatherData> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/weather/current`,
+    `${process.env.NEXT_PUBLIC_URL}/api/weather/current`,
     { cache: "no-store" }
   );
   return res.json();
@@ -10,7 +10,7 @@ export const fetchCurrentWeather = async (): Promise<CurrentWeatherData> => {
 
 export const fetchWeatherForecast = async (): Promise<ForecastWeatherData> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/weather/forecast`,
+    `${process.env.NEXT_PUBLIC_URL}/api/weather/forecast`,
     { cache: "no-store" }
   );
   return res.json();
