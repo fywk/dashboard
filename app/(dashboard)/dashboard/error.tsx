2d9e24ae0ebe87bf21c325702c8efde3a92f09f9
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-01-19 18:16:29 +0800

    Add fallback UIs for `/dashboard` route

---
+++ app/(dashboard)/dashboard/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+// 'use client' marks this page as a Client Component
+// https://beta.nextjs.org/docs/rendering/server-and-client-components
+
+import { useEffect } from "react";
+
+const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <p>Something went wrong!</p>
+      <button onClick={() => reset()}>Reset error boundary</button>
+    </div>
+  );
+};
+
+export default Error;
