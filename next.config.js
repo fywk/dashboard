936f8be4bcad5f6133e3ffda174fb4901ea616c0
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-01-30 02:01:37 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["lastfm.freetls.fastly.net", "i.scdn.co"],
+    formats: ["image/avif", "image/webp"],
+  },
   experimental: {
     appDir: true,
     fontLoaders: [
       { loader: "@next/font/google", options: { subsets: ["latin"] } },
     ],
   },
-  images: {
-    domains: ["lastfm.freetls.fastly.net", "i.scdn.co"],
-    formats: ["image/avif", "image/webp"],
-  },
-  reactStrictMode: true,
-  rewrites: async function () {
+  async rewrites() {
     return [
       {
         source: "/",
+        destination: "/dashboard",
+      },
+      {
+        source: "//",
         destination: "/dashboard",
       },
     ];
