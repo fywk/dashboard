c1724dcab3a9ce0ce373bc1a41899f2e15c026d7
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-02-28 08:30:51 +0800

    Add optional subtitle field on `Section`

--- app/(dashboard)/components/Section.tsx
+++ app/(dashboard)/components/Section.tsx
@@ -2,24 +2,36 @@ import clsx from "clsx";
 
 type Props = {
   title: string;
+  subtitle?: string | number;
   accentColor: "primary" | "secondary";
   children: React.ReactNode;
 };
 
-const Section = ({ title, accentColor, children }: Props) => {
+const Section = ({ title, subtitle, accentColor, children }: Props) => {
   return (
     <section className="mx-auto grid w-full max-w-[800px] grid-rows-[auto_1fr] min-[1440px]:max-w-[1040px]">
-      <h2
+      <div
         className={clsx(
-          "mb-4 border-t-2 border-b py-1 text-lg font-bold uppercase text-primary",
+          "mb-4 border-t-2 border-b py-1",
           accentColor === "primary"
             ? "[border-top-color:rgb(var(--color-primary)/.35)] [border-bottom-color:rgb(var(--color-primary)/1)]"
             : "[border-top-color:rgb(var(--color-secondary)/.35)] [border-bottom-color:rgb(var(--color-secondary)/1)]"
         )}
       >
-        <span className="brightness-[2.5]">{title}</span>
-        <div className="mb-px h-px bg-primary/50"></div>
-      </h2>
+        <div className="flex items-center justify-between font-oxanium">
+          <h2 className="text-lg font-bold">
+            <span className="uppercase text-primary brightness-[2.5]">
+              {title}
+            </span>
+          </h2>
+          {subtitle && (
+            <p className="text-[10px] font-medium text-gray-300 sm:text-xs md:text-sm">
+              {subtitle}
+            </p>
+          )}
+        </div>
+        <div className="mb-px h-px w-full bg-primary/50"></div>
+      </div>
       <div className="px-0.5 sm:px-1">{children}</div>
     </section>
   );
