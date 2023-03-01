13f99ac3bfbbe8989b0367bf64164db549424f87
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-03-01 19:34:42 +0800

    Add missing metadata

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,21 +1,11 @@
 import "./dashboard.css";
 
-import { Metadata } from "next";
 import { Oxanium } from "next/font/google";
-
-import { siteConfig } from "@/lib/utils/site-config";
 
 const oxanium = Oxanium({
   subsets: ["latin"],
   variable: "--font-oxanium",
 });
-
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.title,
-    template: `%s ${siteConfig.titleSeparator} ${siteConfig.title}`,
-  },
-};
 
 const DashboardRootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
