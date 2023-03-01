e791f180cfad269135090ba22e3dd495c23ddbb2
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-03-01 20:00:59 +0800

    Fix missing metadata (#12)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
 import "./globals.css";
 
+import { Metadata } from "next";
+
+import { siteConfig } from "@/lib/utils/site-config";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.title,
+    template: `%s ${siteConfig.titleSeparator} ${siteConfig.title}`,
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    viewportFit: "cover",
+  },
+  icons: {
+    icon: [
+      { url: "/favicons/favicon.ico", sizes: "any" },
+      { url: "/favicons/favicon.svg", type: "image/svg+xml" },
+    ],
+    apple: "/favicons/apple-touch-icon.png",
+  },
+  manifest: "/favicons/site.webmanifest",
+};
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>;
 };
