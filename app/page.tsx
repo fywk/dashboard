e838ca508a1aee955d2b3007e424f7551714db4d
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-05-02 17:00:29 +0800

    Update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 const DashboardPage: NextPage<Props> = ({ searchParams }) => {
   return (
     <main
-      className="mx-auto grid w-full max-w-[1920px] grid-cols-1 gap-x-10 gap-y-7 py-8 md:gap-y-8 min-[1440px]:grid-cols-[55%_auto]"
+      className="mx-auto grid w-full max-w-[1920px] grid-cols-1 gap-x-9 gap-y-7 py-8 md:gap-y-8 min-[1280px]:grid-cols-2 min-[1440px]:grid-cols-[52.5%_auto] min-[1440px]:gap-x-10 min-[1680px]:grid-cols-[55%_auto]"
       id="content"
     >
       <Profile />
