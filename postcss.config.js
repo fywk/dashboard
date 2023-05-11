ade4364a4c2ae4b6e795d68c1e4ffda83c18494e
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-05-11 15:44:36 +0800

    Add `postcss-pxtorem` plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
+    "postcss-pxtorem": {
+      propList: [
+        "font",
+        "font-size",
+        "letter-spacing",
+        "line-height",
+        "margin-top",
+        "margin-right",
+        "margin-bottom",
+        "margin-left",
+        "padding-top",
+        "padding-right",
+        "padding-bottom",
+        "padding-left",
+      ],
+    },
     autoprefixer: {},
   },
-}
+};
