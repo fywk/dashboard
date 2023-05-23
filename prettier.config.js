5216d55c20e8d22a26b6bc102d936691ffd00108
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-05-23 19:48:39 +0800

    Add prettier import sorter

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,17 @@
+/** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [
+    "<THIRD_PARTY_TS_TYPES>",
+    "<TS_TYPES>^[./]",
+    "<THIRD_PARTY_MODULES>",
+    "^[@/]/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
 };
