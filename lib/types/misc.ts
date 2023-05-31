e65538fcd89951ce354b9e231942ac86aa53c9e6
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-05-31 19:20:12 +0800

    Remove extra vars from being set to `searchParams`

--- lib/types/misc.ts
+++ lib/types/misc.ts
@@ -1,6 +1,4 @@
 export type SearchParams = {
   city: string;
   country: string;
-  latitude: string;
-  longitude: string;
 };
--- middleware.ts
+++ middleware.ts
@@ -21,13 +21,9 @@ export function middleware(request: NextRequest) {
   // Default values are set since geo object is only available in production
   const city = geo?.city ?? "Kuala Lumpur";
   const country = geo?.country ?? "Malaysia";
-  const latitude = geo?.latitude ?? "3.1502";
-  const longitude = geo?.longitude ?? "101.7077";
 
   url.searchParams.set("city", city);
   url.searchParams.set("country", country);
-  url.searchParams.set("latitude", latitude);
-  url.searchParams.set("longitude", longitude);
 
   return NextResponse.rewrite(url);
 }
