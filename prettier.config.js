30bd1075dc7758b46cacd2ebf3ccebda4df03424
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-07-02 22:53:44 +0800

    Change prettier import sort plugin (#28)

--- prettier.config.js
+++ prettier.config.js
@@ -1,20 +1,24 @@
 module.exports = {
   plugins: [
-    "@trivago/prettier-plugin-sort-imports",
+    "@ianvs/prettier-plugin-sort-imports",
     "prettier-plugin-tailwindcss",
   ],
-  // @trivago/prettier-plugin-sort-imports
+  pluginSearchDirs: false,
+  // @ianvs/prettier-plugin-sort-imports
   importOrder: [
+    "",
     "<THIRD_PARTY_MODULES>",
+    "",
     "^[@/]/(.*)$",
+    "",
     "^[./]",
-    "<THIRD_PARTY_TS_TYPES>",
-    "<TS_TYPES>^[@/]/(.*)$",
-    "<TS_TYPES>^[./]",
+    "",
+    "<TYPES>",
+    "",
+    "<TYPES>^[@/]/(.*)$",
+    "",
+    "<TYPES>^[./]",
   ],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  importOrderCaseInsensitive: true,
   // prettier-plugin-tailwindcss
   tailwindAttributes: ["customClasses"],
   tailwindFunctions: ["clsx"],
