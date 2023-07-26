200e77e593138a39129e0b02a7fb5f233b52358d
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-07-27 00:02:24 +0800

    Change divs within `Music` to sections

--- app/components/Music.tsx
+++ app/components/Music.tsx
@@ -24,28 +24,28 @@ export default function Music() {
   return (
     <Section title="Music" accentColor="secondary">
       <div className="grid grid-cols-1 gap-3.5 @lg/section:grid-cols-2 @lg/section:gap-x-7 @xl/section:gap-x-7.5 @1.5xl/section:gap-x-8 @2xl/section:gap-x-9">
-        <div className="grid grid-rows-[auto_1fr] @container/now-playing @lg/section:order-2">
+        <section className="grid grid-rows-[auto_1fr] @container/now-playing @lg/section:order-2">
           <h3 className={SUBTITLE_STYLE}>Recently Played</h3>
           <NowPlaying />
-        </div>
-        <div className="grid grid-rows-[auto_1fr] @lg/section:order-1">
+        </section>
+        <section className="grid grid-rows-[auto_1fr] @lg/section:order-1">
           <h3 className={SUBTITLE_STYLE}>
             Statistics <small className="pl-0.5">{humanizePeriod(timeRange.statistics)}</small>
           </h3>
           <Statistics period={timeRange.statistics} />
-        </div>
-        <div className="@lg/section:order-3 @lg/section:col-span-full">
+        </section>
+        <section className="@lg/section:order-3 @lg/section:col-span-full">
           <h3 className={SUBTITLE_STYLE}>
             Top Artists <small className="pl-0.5">{humanizePeriod(timeRange.artists)}</small>
           </h3>
           <TopArtistsGrid period={timeRange.artists} />
-        </div>
-        <div className="w-full @lg/section:order-5 @lg/section:col-span-full">
+        </section>
+        <section className="w-full @lg/section:order-5 @lg/section:col-span-full">
           <h3 className={SUBTITLE_STYLE}>
             Top Albums <small className="pl-0.5">{humanizePeriod(timeRange.albums)}</small>
           </h3>
           <TopAlbumsGrid period={timeRange.albums} />
-        </div>
+        </section>
       </div>
     </Section>
   );
