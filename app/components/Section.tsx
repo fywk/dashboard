1b1fadee70476180c4f21ae2c6409b2520ba75e9
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-08-04 13:45:43 +0800

    Use `<hgroup>` element

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -22,12 +22,12 @@ export default function Section({ title, subtitle, accentColor, customClasses, c
           "border-b border-t-2 py-1 [border-bottom-color:rgb(var(--border-accent-color)/1)] [border-top-color:rgb(var(--border-accent-color)/.35)]",
         )}
       >
-        <div className="flex items-center justify-between border-y border-b-primary/50 border-t-transparent px-px py-[5px] xs:px-0.5">
+        <hgroup className="flex items-center justify-between border-y border-b-primary/50 border-t-transparent px-px py-[5px] xs:px-0.5">
           <h2 className="font-oxanium text-[17px] font-[650] uppercase !leading-none text-gray-50 [text-shadow:0_0_0.5rem_rgb(var(--color-primary))] @1.5xl/quadrant:text-lg @1.5xl/section:text-lg">
             {title}
           </h2>
           {subtitle}
-        </div>
+        </hgroup>
       </div>
       <div className="px-0.5 @1.5xl/quadrant:px-1 @1.5xl/section:px-1">{children}</div>
     </section>
