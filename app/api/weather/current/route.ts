fdfb8fd3c776a8ff7c4f629c8b6bab1922b06d62
Francis Yeong <33077075+fywk@users.noreply.github.com>
2026-01-14 01:00:44 +0800

    Migrate all API Routes to Route Handlers (#64)

--- app/api/weather/current/route.ts
+++ app/api/weather/current/route.ts
@@ -5,29 +5,15 @@ import { getCurrentWeather } from "@/lib/services/openweather";
 
 import type { NextRequest } from "next/server";
 
-export const config = {
-  runtime: "edge",
-};
-
-export default async function handler(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const latitude = geolocation(request).latitude ?? app.location.latitude;
   const longitude = geolocation(request).longitude ?? app.location.longitude;
 
   const currentWeather = await getCurrentWeather(latitude, longitude);
 
   if (!currentWeather) {
-    return new Response(JSON.stringify({ error: "An error occurred while trying to fetch." }), {
-      status: 400,
-      headers: {
-        "content-type": "application/json",
-      },
-    });
+    return Response.json({ error: "An error occurred while trying to fetch." }, { status: 400 });
   }
 
-  return new Response(JSON.stringify(currentWeather), {
-    status: 200,
-    headers: {
-      "content-type": "application/json",
-    },
-  });
+  return Response.json(currentWeather);
 }
